b789cbdaf1a6e2066abb09b074e2da2cbf99d998
jameskim <jameskim9509@github.com>
2024-12-18 09:26:21 +0900

    [Feat] 포인트 충전/사용에 대한 실패 케이스 테스트 코드 작성

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
@@ -23,16 +24,14 @@ class PointServiceTest {
     PointService pointService;
 
     @BeforeEach
-    void setup()
-    {
+    void setup() {
         MockitoAnnotations.openMocks(this);
     }
 
     /**
      * 포인트 충전
      */
-    private void assertChargePointEqual(Long userId, Long beforePoint, Long chargePoint, Long expectedPoint)
-    {
+    private void assertChargePointEqual(Long userId, Long beforePoint, Long chargePoint, Long expectedPoint) {
         // given
         doReturn(new UserPoint(userId, beforePoint, 0)).when(userPointTable).selectById(userId);
         doReturn(new UserPoint(userId, beforePoint + chargePoint, System.currentTimeMillis())).when(userPointTable).insertOrUpdate(userId, beforePoint + chargePoint);
@@ -40,7 +39,8 @@ class PointServiceTest {
         ArgumentCaptor<Long> userIdCaptor1 = ArgumentCaptor.forClass(Long.class);
         ArgumentCaptor<Long> leftPointAmountCaptor = ArgumentCaptor.forClass(Long.class);
         ArgumentCaptor<Long> userIdCaptor2 = ArgumentCaptor.forClass(Long.class);
-        ArgumentCaptor<Long> chargedPointAmountCaptor = ArgumentCaptor.forClass(Long.class);;
+        ArgumentCaptor<Long> chargedPointAmountCaptor = ArgumentCaptor.forClass(Long.class);
+        ;
         ArgumentCaptor<TransactionType> transactionTypeCaptor = ArgumentCaptor.forClass(TransactionType.class);
         ArgumentCaptor<Long> updateMillisCaptor = ArgumentCaptor.forClass(Long.class);
 
@@ -68,8 +68,7 @@ class PointServiceTest {
     /**
      * 포인트 사용
      */
-    private void assertUsePointEqual(Long userId, Long beforePoint, Long usePoint, Long expectedPoint)
-    {
+    private void assertUsePointEqual(Long userId, Long beforePoint, Long usePoint, Long expectedPoint) {
         // given
         doReturn(new UserPoint(userId, beforePoint, 0)).when(userPointTable).selectById(userId);
         doReturn(new UserPoint(userId, beforePoint - usePoint, System.currentTimeMillis())).when(userPointTable).insertOrUpdate(userId, beforePoint - usePoint);
@@ -77,7 +76,8 @@ class PointServiceTest {
         ArgumentCaptor<Long> userIdCaptor1 = ArgumentCaptor.forClass(Long.class);
         ArgumentCaptor<Long> leftPointAmountCaptor = ArgumentCaptor.forClass(Long.class);
         ArgumentCaptor<Long> userIdCaptor2 = ArgumentCaptor.forClass(Long.class);
-        ArgumentCaptor<Long> usedPointAmountCaptor = ArgumentCaptor.forClass(Long.class);;
+        ArgumentCaptor<Long> usedPointAmountCaptor = ArgumentCaptor.forClass(Long.class);
+        ;
         ArgumentCaptor<TransactionType> transactionTypeCaptor = ArgumentCaptor.forClass(TransactionType.class);
         ArgumentCaptor<Long> updateMillisCaptor = ArgumentCaptor.forClass(Long.class);
 
@@ -101,20 +101,52 @@ class PointServiceTest {
         Mockito.clearInvocations(userPointTable);
         Mockito.clearInvocations(pointHistoryTable);
     }
-    
+
     @DisplayName("포인트 충전 성공")
     @Test
-    void chargePointSuccess()
-    {
+    void chargePointSuccess() {
         assertChargePointEqual(1L, 1000L, 1000L, 2000L);
         assertChargePointEqual(2L, 1500L, 2000L, 3500L);
     }
 
     @DisplayName("포인트 사용 성공")
     @Test
-    void usePointSuccess()
-    {
+    void usePointSuccess() {
         assertUsePointEqual(1L, 1000L, 600L, 400L);
         assertUsePointEqual(2L, 2000L, 1500L, 500L);
+    }
+
+    /**
+     * 포인트 최대 잔고 초과
+     */
+    @DisplayName("포인트 최대 잔고 초과")
+    @Test
+    void 포인트_최대_잔고_초과() {
+        // given
+        doReturn(new UserPoint(1L, PointService.MAX_POINT, 0)).when(userPointTable).selectById(1L);
+        doReturn(new UserPoint(1L, PointService.MAX_POINT + 10000L, System.currentTimeMillis())).when(userPointTable).insertOrUpdate(1L, PointService.MAX_POINT + 10000L);
+
+        // when, then
+        assertThrows(
+                RuntimeException.class,
+                () -> pointService.chargePoint(1L, 10000L)
+        );
+    }
+
+    /**
+     * 포인트 잔고 부족
+     */
+    @DisplayName("포인트 잔고 부족")
+    @Test
+    void 포인트_잔고_부족() {
+        // given
+        doReturn(new UserPoint(1L, 0L, 0)).when(userPointTable).selectById(1L);
+        doReturn(new UserPoint(1L, -1000L, System.currentTimeMillis())).when(userPointTable).insertOrUpdate(1L, -1000L);
+
+        // when, then
+        assertThrows(
+                RuntimeException.class,
+                () -> pointService.usePoint(1L, 1000L)
+        );
     }
 }
